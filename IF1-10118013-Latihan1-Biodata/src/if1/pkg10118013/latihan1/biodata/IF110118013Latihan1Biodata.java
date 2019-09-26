99453a5280498891666e5bf59e08cbd378021df7
RifianJoe <55788355+RifianJoe@users.noreply.github.com>
2019-09-26 15:01:03 +0700

    Add files via upload

---
+++ IF1-10118013-Latihan1-Biodata/src/if1/pkg10118013/latihan1/biodata/IF110118013Latihan1Biodata.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118013.latihan1.biodata;
+
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ *
+ * @author Rifian
+ * Nama : Rifian Joe Premilenio
+ * Kelas : IF-1
+ * NIM : 10118013
+ * Deskripsi Program : Program ini berisi program untuk menampilkan jenis-jenis tipe data bilangan bulat
+ */
+public class IF110118013Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "----------------------------------------");
+        System.out.println("NAMA\t\t: Rifian Joe Premilenio");
+        System.out.println("ALAMAT \t\t: Jalan Abdul Hamid");
+        System.out.println("TEMPAT LAHIR\t: Merauke");
+        System.out.println("TANGGAL LAHIR\t: 05 Juli 2000");
+        System.out.println("----------------------------------------");
+        System.out.println("HOBI\t\t: Nonton Film");
+        System.out.println("MAKES\t\t: Nasi");
+        System.out.println("MIKES\t\t: Big Ice Tea");
+    }
+    
+}
